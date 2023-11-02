f193bd92d6f5446bdef043a601fea6028e127939
YasmimLira <micayaya70@gmail.com>
2023-11-02 20:42:59 -0300

    Adicionando novas seções e scripts

---
+++ script.js
@@ -0,0 +1,30 @@
+AOS.init();
+
+// Detecta quando o usuário rola a página
+window.onscroll = function() {
+    scrollFunction();
+};
+
+// Mostra ou oculta os botões com base na posição da rolagem
+function scrollFunction() {
+    const scrollToTopButtons = document.querySelectorAll(".scrollToTopButton");
+    
+    scrollToTopButtons.forEach(function(button) {
+        if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+            button.style.display = "block";
+        } else {
+            button.style.display = "none";
+        }
+    });
+}
+
+// Rola para o topo da página quando um botão é clicado
+const scrollToTopButtons = document.querySelectorAll(".scrollToTopButton");
+scrollToTopButtons.forEach(function(button) {
+    button.addEventListener("click", function() {
+        document.body.scrollTop = 0; // Para navegadores da web
+        document.documentElement.scrollTop = 0; // Para IE/Edge
+    });
+});
+
+
